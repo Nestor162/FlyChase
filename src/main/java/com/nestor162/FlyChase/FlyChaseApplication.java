3e317914537742dcfb76e08389b7c852fa47d8c9
Nestor162 <113930607+Nestor162@users.noreply.github.com>
2025-02-10 15:22:16 +0100

    Initialize spring

---
+++ src/main/java/com/nestor162/FlyChase/FlyChaseApplication.java
@@ -0,0 +1,13 @@
+package com.nestor162.FlyChase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyChaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyChaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestor162/FlyChase/FlyChaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestor162.FlyChase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyChaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
